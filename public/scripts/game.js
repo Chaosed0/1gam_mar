57188ead1ed2a4dc1d7958996c279c8052d1a831
Ed Lu <chaosed0@gmail.com>
2015-03-23 20:20:44 -0400

    Lose condition

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -3,12 +3,17 @@ define(['crafty', 'jquery', './Util',
         './Circle',
         './Shape',
     ], function(Crafty, $, u) {
+
+    const tweenTime = 2000;
+    const easingFunc = "easeInQuad";
+
     var self = this;
-    var map;
     
     var width = $(window).width();
     var height = $(window).height();
     var gameElem = document.getElementById('game');
+
+    var lost = false;
 
     Crafty.init(width, height, gameElem);  			  		
 
@@ -58,8 +63,7 @@ define(['crafty', 'jquery', './Util',
         Crafty.background("#AAAAAA");
 
         var shrink1 = Crafty.e("2D, Canvas, Shape, Tween")
-            .fillcolor("#AAAAAA")
-            .tween({x: width/2 - 20, y: height/2 - 20, w: 40, h: 40}, 2000, "EaseInOut");
+            .fillcolor("#AAAAAA");
 
         var shrink2 = Crafty.e("2D, Canvas, Shape, Tween")
             .fillcolor("#AAAAAA");
@@ -67,37 +71,64 @@ define(['crafty', 'jquery', './Util',
         randomShape(shrink1);
         randomShape(shrink2);
 
+        var playerShape = Crafty.e("2D, Canvas, Shape")
+            .attr({ x: width/2 - 20, y: height/2 - 20, w: 40, h: 40, z: 1000 })
+            .sides(3)
+            .fillcolor("#EEEEEE");
+
+        var tweenToPlayer = function(shape) {
+            shape.tween({x: playerShape.x, y: playerShape.y, w: playerShape.w, h: playerShape.h},
+                    tweenTime, easingFunc);
+        }
+
         var tweenEnd = function() {
             var ender = (this === shrink1 ? shrink1 : shrink2);
             var starter = (this === shrink1 ? shrink2 : shrink1);
 
-            ender.z = 0;
-            randomShape(ender);
+            if (ender.sides() != playerShape.sides()) {
+                /* Lose */
+                lost = true;
+                console.log("loss: " + ender.sides());
+                Crafty.pause();
+            } else {
+                ender.z = 0;
+                randomShape(ender);
 
-            starter.z = 1;
-            starter.tween({x: width/2 - 20, y: height/2 - 20, w: 40, h: 40}, 2000, "easeInQuad");
+                starter.z = 1;
+                tweenToPlayer(starter);
+            }
         };
 
         shrink1.bind("TweenEnd", tweenEnd);
         shrink2.bind("TweenEnd", tweenEnd);
 
+        /* Total hack to destroy Crafty's HashMap.refresh() callback, which performs
+         * really horribly on larger entities
+         * This has the side effect of making collisions totally unusable, which
+         * shouldn't be a problem for this game */
         shrink1.unbind("Move");
         shrink2.unbind("Move");
 
-        var shape = Crafty.e("2D, Canvas, Shape")
-            .attr({ x: width/2 - 20, y: height/2 - 20, w: 40, h: 40, z: 1000 })
-            .sides(3)
-            .fillcolor("#EEEEEE");
+        /* Start one of the shapes tweening toward the player */
+        tweenToPlayer(shrink1);
+        shrink1.z = 1;
+        shrink2.z = 0;
 
         var keyDownHandler = function(e) {
-            var sides = shape.sides();
-            if (e.key === Crafty.keys.LEFT_ARROW) {
-                if (sides > 3) {
-                    shape.sides(sides-1);
-                }
-            } else if (e.key === Crafty.keys.RIGHT_ARROW) {
-                if (sides < 8) {
-                    shape.sides(sides+1);
+            if (lost) {
+                lost = false;
+                Crafty.scene("Main");
+                Crafty.pause();
+            } else {
+                var sides = playerShape.sides();
+                if (e.key === Crafty.keys.LEFT_ARROW) {
+                    if (sides > 3) {
+                        playerShape.sides(sides-1);
+                    }
+                } else if (e.key === Crafty.keys.RIGHT_ARROW) {
+                    if (sides < 8) {
+                        playerShape.sides(sides+1);
+                    }
                 }
             }
         }
